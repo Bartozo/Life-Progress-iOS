b71cb5e1594a2148e92a71fece518425e5c70b85
Bartozo <bartozo.dev@gmail.com>
2023-06-17 12:44:20 +0200

    ✅ Added developer feature tests

---
+++ LifeProgressTests/Features/Developer/DeveloperTests.swift
@@ -0,0 +1,52 @@
+//
+//  DeveloperTests.swift
+//  LifeProgressTests
+//
+//  Created by Bartosz Król on 17/06/2023.
+//
+
+import XCTest
+import ComposableArchitecture
+
+@testable import Life_Progress___Calendar
+
+@MainActor
+class DeveloperTests: XCTestCase {
+    
+    func testDeveloperButtonTapped_ShouldShowConfetti() async {
+        let store = TestStore(
+            initialState: DeveloperReducer.State(),
+            reducer: DeveloperReducer()
+        )
+        
+        await store.send(.developerButtonTapped) {
+            $0.confetti = 1
+        }
+    }
+    
+    func testDeveloperButtonTapped_ShouldAddToAnalytics() async {
+        var sentEvent = ""
+        let store = TestStore(
+            initialState: DeveloperReducer.State(),
+            reducer: DeveloperReducer()
+        ) {
+            $0.analyticsClient.send = { event in sentEvent = event }
+        }
+        
+        await store.send(.developerButtonTapped) {
+            $0.confetti = 1
+        }
+        XCTAssertEqual(sentEvent, "developer.developer_button_tapped")
+    }
+    
+    func testConfettiChanged_ShouldUpdateConfettiState() async {
+        let store = TestStore(
+            initialState: DeveloperReducer.State(confetti: 1),
+            reducer: DeveloperReducer()
+        )
+        
+        await store.send(.confettiChanged(0)) {
+            $0.confetti = 0
+        }
+    }
+}
