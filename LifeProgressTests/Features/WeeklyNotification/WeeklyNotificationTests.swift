aaac1a9cdca7f92b0dec04f6c4f4edcb578e5fcd
Bartozo <bartozo.dev@gmail.com>
2023-06-17 16:37:43 +0200

    ✅ Added weekly notification feature tests

---
+++ LifeProgressTests/Features/WeeklyNotification/WeeklyNotificationTests.swift
@@ -0,0 +1,59 @@
+//
+//  WeeklyNotificationTests.swift
+//  LifeProgressTests
+//
+//  Created by Bartosz Król on 17/06/2023.
+//
+
+import XCTest
+import ComposableArchitecture
+
+@testable import Life_Progress___Calendar
+
+@MainActor
+class WeeklyNotificationTests: XCTestCase {
+    
+    func testIsWeeklyNotificationChanged_ShouldBeEnabled() async {
+        let store = TestStore(
+            initialState: WeeklyNotificationReducer.State(),
+            reducer: WeeklyNotificationReducer()
+        )
+        
+        await store.send(.isWeeklyNotificationChanged) {
+            $0.isWeeklyNotificationEnabled = true
+        }
+    }
+    
+    func testIsWeeklyNotificationChanged_ShouldBeDisabled() async {
+        let store = TestStore(
+            initialState: WeeklyNotificationReducer.State(isWeeklyNotificationEnabled: true),
+            reducer: WeeklyNotificationReducer()
+        )
+        
+        await store.send(.isWeeklyNotificationChanged) {
+            $0.isWeeklyNotificationEnabled = false
+        }
+    }
+    
+    func testIsWeeklyNotificationChanged_ShouldAddToAnalytics() async {
+        var eventName = ""
+        var eventPayload = [String: String]()
+        let store = TestStore(
+            initialState: WeeklyNotificationReducer.State(),
+            reducer: WeeklyNotificationReducer()
+        ) {
+            $0.analyticsClient.sendWithPayload = { event, payload in
+                eventName = event
+                eventPayload = payload
+            }
+        }
+        
+        await store.send(.isWeeklyNotificationChanged) {
+            $0.isWeeklyNotificationEnabled = true
+        }
+        
+        XCTAssertEqual(eventName, "weekly_notification.is_weekly_notification_changed")
+        XCTAssertEqual(eventPayload, ["isWeeklyNotificationEnabled": "true"])
+    }
+}
+
