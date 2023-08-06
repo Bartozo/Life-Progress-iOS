d4f5b6f3006347dc8cacbba05815afeb8778ed66
Bartozo <bartozo.dev@gmail.com>
2023-08-06 15:13:16 +0200

    ✨ Added color picker feature

---
+++ LifeProgress/Features/Shared/ColorPicker/ColorPickerStore.swift
@@ -0,0 +1,76 @@
+//
+//  ColorPickerStore.swift
+//  Life Progress - Calendar
+//
+//  Created by Bartosz Król on 06/08/2023.
+//
+
+import SwiftUI
+import Foundation
+import ComposableArchitecture
+
+/// A type alias for a store of the `ColorPickerReducer`'s state and action types.
+typealias ColorPickerStore = Store<ColorPickerReducer.State, ColorPickerReducer.Action>
+
+/// A reducer that manages the state of the color picker.
+struct ColorPickerReducer: ReducerProtocol {
+    
+    /// The state of the color picker.
+    struct State: Equatable {
+        
+        /// Represents the currently selected color.
+        var color = Color.red
+        
+        /// An enumeration representing the colors.
+        enum Color: Equatable, CaseIterable, Identifiable {
+            case red
+            case orange
+            case yellow
+            case green
+            case blue
+            case purple
+            case pink
+            case gray
+            
+            var id: Color { self }
+            
+            var colorValue: SwiftUI.Color {
+                switch self {
+                case .red:
+                    return .red
+                case .orange:
+                    return .orange
+                case .yellow:
+                    return .yellow
+                case .green:
+                    return .green
+                case .blue:
+                    return .blue
+                case .purple:
+                    return .purple
+                case .pink:
+                    return .pink
+                case .gray:
+                    return .gray
+                }
+            }
+        }
+    }
+    
+    /// The actions that can be taken on the color picker.
+    enum Action: Equatable {
+        /// Indicates that color has changed.
+        case colorChanged(State.Color)
+    }
+    
+    /// The body of the reducer that processes incoming actions and updates the state accordingly.
+    var body: some ReducerProtocol<State, Action> {
+        Reduce { state, action in
+            switch action {
+            case .colorChanged(let color):
+                state.color = color
+                return .none
+            }
+        }
+    }
+}
---
+++ LifeProgress/Features/Shared/ColorPicker/ColorPickerView.swift
@@ -0,0 +1,59 @@
+//
+//  ColorPickerView.swift
+//  Life Progress - Calendar
+//
+//  Created by Bartosz Król on 06/08/2023.
+//
+
+import SwiftUI
+import ComposableArchitecture
+
+struct ColorPickerView: View {
+    
+    @Environment(\.theme) var theme
+    
+    let store: ColorPickerStore
+    
+    private var gridItems: [GridItem] {
+        [GridItem(.adaptive(minimum:50, maximum: 70))]
+    }
+    
+    var body: some View {
+        WithViewStore(self.store, observe: \.color) { viewStore in
+            LazyVGrid(columns: gridItems, spacing: 8) {
+                ForEach(ColorPickerReducer.State.Color.allCases) { color in
+                    ZStack {
+                        if viewStore.state == color {
+                            Circle()
+                                .stroke(color.colorValue, lineWidth: 3)
+                                .frame(width: 50, height: 50)
+                        }
+                        
+                        Circle()
+                           .fill(color.colorValue.gradient)
+                           .frame(width: 40, height: 40)
+                           .tag(color)
+                           .onTapGesture {
+                               viewStore.send(.colorChanged(color))
+                           }
+                    }
+                    .frame(width: 50, height: 50)
+                }
+            }
+        }
+    }
+}
+
+// MARK: - Previews
+
+struct ColorPickerView_Previews: PreviewProvider {
+    
+    static var previews: some View {
+        let store = Store<ColorPickerReducer.State, ColorPickerReducer.Action>(
+            initialState: ColorPickerReducer.State(),
+            reducer: ColorPickerReducer()
+        )
+        
+        ColorPickerView(store: store)
+    }
+}
