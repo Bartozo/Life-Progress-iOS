7a40207c7e8a80272356988315720363a4411d32
Bartozo <bartozo.dev@gmail.com>
2024-01-04 15:42:16 +0100

    🎨 Updated code for SF symbol picker feature

--- LifeProgress/Features/Shared/SFSymbolPicker/SFSymbolPickerStore.swift
+++ LifeProgress/Features/Shared/SFSymbolPicker/SFSymbolPickerStore.swift
@@ -9,41 +9,36 @@ import Foundation
 import ComposableArchitecture
 
 /// A reducer that manages the state of the SF Symbol picker.
-struct SFSymbolPickerReducer: Reducer {
+@Reducer
+struct SFSymbolPickerReducer {
     
     /// The state of the about the app.
     struct State: Equatable {
         /// The selected SF Symbol for the life goal. By default set to "trophy"
-        var symbolName = "trophy"
+        @BindingState var symbolName = "trophy"
         
         /// Whether the SF Symbol sheet is visible.
-        var isSheetVisible = false
+        @BindingState var isSheetVisible = false
     }
     
     /// The actions that can be taken on the about the app.
-    enum Action: Equatable {
-        /// Indicates that is SF Symbol has changed.
-        case symbolNameChanged(String)
+    enum Action: BindableAction, Equatable {
+        /// The binding for the SF Symbol Picker.
+        case binding(BindingAction<State>)
         /// Indicates that the sheet should be visible.
         case showSheet
-        /// Indicates that the sheet should be hidden.
-        case hideSheet
     }
     
     /// The body of the reducer that processes incoming actions and updates the state accordingly.
     var body: some Reducer<State, Action> {
+        BindingReducer()
         Reduce { state, action in
             switch action {
-            case .symbolNameChanged(let symbolName):
-                state.symbolName = symbolName
+            case .binding(_):
                 return .none
                 
             case .showSheet:
                 state.isSheetVisible = true
-                return .none
-                
-            case .hideSheet:
-                state.isSheetVisible = false
                 return .none
             }
         }
